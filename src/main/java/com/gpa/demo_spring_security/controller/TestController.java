0422d9ce6ef735227b16124a50376fd978c75e3e
peter <anhnlam051595@gmail.com>
2025-05-12 16:52:59 -0400

    Initial commit

---
+++ src/main/java/com/gpa/demo_spring_security/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.gpa.demo_spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String index() {
+        return "test";
+    }
+}
